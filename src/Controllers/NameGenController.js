5ff4178f7776bf015183b6e1a810389888511516
JLucas <joaolucaspachecoab@gmail.com>
2019-10-09 20:04:29 -0300

    Random name generator created

--- src/Controllers/NameGenController.js
+++ src/Controllers/NameGenController.js
@@ -1,8 +1,8 @@
+const axios = require("axios")
+const express = require('express')
 const nameList = require("../../assets/names.json")
 
-
-module.exports = {
-    generateName( ){
+function generateName( ){
 
     names = nameList.names
     
@@ -17,5 +17,34 @@ module.exports = {
     return firstName.concat(secondName)
 }
 
+async function validateDomain(domainName){
 
+    await axios.get('https://api.jsonwhois.io/availability?key=fwAo7rPkK7HPnj3fuZHzFpKSzQNeQ0wO&domain=' + domainName.toLowerCase() + '.com')
+    .then(res => {
+        const isAvailable = res.data.is_available
+    })
+    .catch(err => {
+        console.log(err);
+    });
+    
+    return isAvailable
+}
+
+module.exports = {
+    
+    generateNameList(req, res){
+        const names = []
+        const { listSize } = req.headers
+        
+        for(i = 0, i < listSize, i++){
+            const name = generateName()
+            const availability = validateDomain(name)
+            names.push({
+                name,
+                availability
+            })
+        }
+
+        return names
+    }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const nameGenerator = require("./Controllers/NameGenController")
 
 const routes = express.Router()
 
 routes.get('/names', (req, res) => {
+
+    const startupNameList = nameGenerator.generateNameList(req.body.listSize)
+
     
 })
